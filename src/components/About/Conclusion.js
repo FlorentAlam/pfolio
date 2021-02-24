6c1c7a2951e1a8019112ac9e187c5cd5c6b23e8d
Florent Alamachere <florentalamachere@yahoo.fr>
2021-02-24 18:30:28 +0100

    Modified about footer animation to trigger on scroll

--- src/components/About/Conclusion.js
+++ src/components/About/Conclusion.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import lang from '../../utils/lang';
 import { LangContext } from '../App/AppProvider';
@@ -6,8 +6,23 @@ import { LangContext } from '../App/AppProvider';
 const Conclusion = () => {
     const langage = useContext(LangContext);
 
+    const content = useRef(null);
+
+    const [isActive, setActive] = useState(false);
+
+    useEffect(() => {
+        const {top} = content.current.getBoundingClientRect();
+
+        window.addEventListener('scroll', () => {
+            console.log(window.scrollY);
+            if(!isActive){
+                if(window.scrollY >= top) setActive(true);
+            }
+        })
+    }, [isActive]);
+
     return(
-    <h1 className="about-large-text about-conclusion">
+    <h1 className={"about-large-text about-conclusion " + (isActive ? "conclusion--active" : "")} ref={content}>
         <span>{lang.about.alors[langage.langage]}</span> 
         <span>{lang.about.dowe[langage.langage]}</span>
         <span><Link to="/contact">{lang.about.contact[langage.langage]}</Link> ?</span>
